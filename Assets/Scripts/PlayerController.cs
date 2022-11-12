7f92a3bd96e6a52fc27d9ac0fc1fd744e9970299
seorin1215 <seorin1231@gmail.com>
2022-11-12 19:55:50 +0900

    PlayerController 버그 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 700f;
     public float runSpeed = 2.0f;
     private int jumpCount = 0;
-    private bool isGrounded = false;
     private bool isJumping = false;
     private bool isDoubleJump = false;
     private Rigidbody2D playerRigidbody;
@@ -37,34 +36,17 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-
-
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-        //Landing Paltform
-        Debug.DrawRay(playerRigidbody.position, Vector3.down, new Color(0, 1, 0));
-
-        RaycastHit2D rayHit = Physics2D.Raycast(playerRigidbody.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-
-
-
-        if (playerRigidbody.velocity.y < 0)
-        { // 뛰어올랐다가 아래로 떨어질 때만 빔을 쏨 
-            if (rayHit.collider != null)
-            { //빔을 맞은 오브젝트가 있을때  -> 맞지않으면 collider도 생성되지않음 
-                if (rayHit.distance < 2.0f)
-                {
-                    jumpCount = 0; //거리가 0.5보다 작아지면 jumpCount 초기화
-                    isJumping = false;
-                }
-
-
-            }
+        // y축 속도가 0이면 바닥인 것으로 판단
+        if(playerRigidbody.velocity.y == 0.0)
+        {
+            this.isJumping = false;
+            this.jumpCount = 0;
         }
+
+
+
     }
+
+
 
 }
