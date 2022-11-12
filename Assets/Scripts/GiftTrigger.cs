4503bef214e17c13289b0a724925891e103d9ba7
chaeyeon <321codus@naver.com>
2022-11-12 13:25:06 +0900

    gift trigger -> score updatea

---
+++ Assets/Scripts/GiftTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GiftTrigger : MonoBehaviour
+{
+    public GameObject character;
+    public TextMeshProUGUI score_text;
+    public int score; //pink = 1, green = 10
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_text = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.gameObject.tag == "pinkGift")
+        //    score_text.GetComponent<IncreaseScore>().AddScore(1); //pink gift socre = 1
+        //else if(collision.gameObject.tag == "greenGift")
+        //    score_text.GetComponent<IncreaseScore>().AddScore(10); //green gift socre = 10
+        if(collision.gameObject.name == "Character")
+            score_text.GetComponent<IncreaseScore>().AddScore(score);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/IncreaseScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IncreaseScore : MonoBehaviour
+{
+    public TextMeshProUGUI score_text;
+    int score_sum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_text.text = "Score : " + score_sum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int score)
+    {
+        score_sum += score;
+        score_text.text = "Score : " + score_sum;
+    }
+}
